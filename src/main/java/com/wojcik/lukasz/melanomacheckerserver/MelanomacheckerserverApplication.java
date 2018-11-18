2f4a5c15bcb7be291d0a1ece6639758f620f8388
lukaszwojcik <226014@student.pwr.edu.pl>
2018-11-18 08:57:20 +0100

    Initial commit

---
+++ src/main/java/com/wojcik/lukasz/melanomacheckerserver/MelanomacheckerserverApplication.java
@@ -0,0 +1,12 @@
+package com.wojcik.lukasz.melanomacheckerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelanomacheckerserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelanomacheckerserverApplication.class, args);
+    }
+}
